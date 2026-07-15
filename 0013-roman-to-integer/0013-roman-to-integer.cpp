d0e93f0d17459df7b8ebe919d5a4be414c97f343
Shashwat Sinha <shashwatsinha0x@gmail.com>
2026-07-15 23:47:46 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.72%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int out=0,i=0;
+        while(i<s.length()){
+            if(s[i]=='C'){
+                if(s[i+1]=='D'){
+                    out+=400;
+                    i++;
+                }
+                else if(s[i+1]=='M'){
+                    out+=900;
+                    i++;
+                }
+                else{
+                    out+=100;
+
+                }
+                i++;
+            }
+            else if(s[i]=='X'){
+                if(s[i+1]=='L'){
+                    out+=40;
+                    i++;
+                }
+                else if(s[i+1]=='C'){
+                    out+=90;
+                    i++;
+                }
+                else{
+                    out+=10;
+                }
+            i++;    
+            }
+            else if(s[i]=='I'){
+                if(s[i+1]=='V'){
+                    out+=4;
+                    i++;
+                }
+                else if(s[i+1]=='X'){
+                    out+=9;
+                    i++;
+                }
+                else{
+                    out+=1;
+                }
+            i++;    
+            }
+            else if(s[i]=='V'){
+                out+=5;
+                i++;
+            }
+            else if(s[i]=='L'){
+                out+=50;
+                i++;
+            }
+            else if(s[i]=='D'){
+                out+=500;
+                i++;
+            }
+            else{
+                out+=1000;
+                i++;
+            }
+            
+        }
+        return out;
+    }
+};
