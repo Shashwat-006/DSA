9d9f31cc070c29f2b6619b74bfd376e3c0ac4dec
Shashwat Sinha <shashwatsinha0x@gmail.com>
2026-09-22 23:27:22 +0530

    Add solution: 503. Next Greater Element II

---
+++ 503. Next Greater Element II/503. Next Greater Element II.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem: 503. Next Greater Element II
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/next-greater-element-ii/submissions/2150081220/
+ * Language: cpp
+ * Date: 2026-09-22
+ */
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n= nums.size();
+        vector<int> ans(n,-1);
+        stack <int> st;
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int idx = i % n;
+            while (!st.empty() && st.top() <= nums[idx]) {
+                st.pop();
+            }
+            if (i < n) {
+                if (!st.empty()) {
+                    ans[i] = st.top();
+                }
+            }
+            st.push(nums[idx]);
+        }
+        return ans;
+    }
+};
